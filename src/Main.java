fcd576ab12c0b9c53c5bfaa24d74ac92521e0f1c
Ketan Bhatiya <ketanbhatiya111@gmail.com>
2023-11-27 16:50:19 +0530

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import  java.sql.*;
+
 public class Main {
+    private static final String url="jdbc:mysql://localhost:3306/student";
+    private  static  final  String username="root";
+    private static final String password="ket@n";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            Connection connection= DriverManager.getConnection(url,username,password);
+            Statement statement= connection.createStatement();
+            String query= "select *from stu_info";
+            ResultSet resultSet= statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int id= resultSet.getInt("tId");
+                String name= resultSet.getString("tName");
+                String city= resultSet.getString("tCity");
+
+                System.out.println("ID:"+id+" Name:" + name+" City :"+city);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
